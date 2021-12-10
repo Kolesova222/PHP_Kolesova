9aed58fbaf5a55ba0faa2ae086dec865548133ed
Kolesova222 <80149209+Kolesova222@users.noreply.github.com>
2021-12-10 17:14:46 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,59 @@
+<p> Дата и время:
+<p>
+<?php
+ $d=date("d.m.Y H:i");
+ echo($d);
+?>
+<BODY BGCOLOR="#FFFAFA">
+<p><b><CENTER><FONT SIZE="+20"><font color="#EB5284">♥ Колесова Валерия ПИ-322 ♥</font></b></p></FONT></CENTER>
+<CENTER><font color="#4B0082"><FONT SIZE="+2"><I><p>♥ Лабораторная работа 1 ♥</p></I></FONT></font></CENTER>
+
+<CENTER>
+<font color="#BC8F8F">
+<li>
+<a href='/lab-1-1.php'>Упражнение 1-2</a>
+</li>
+<li>
+<a href='/lab-1-2.php'>Упражнение 1-3</a>
+</li>
+<li>
+<a href='/lab-1-3.php'>Упражнение 1-4</a>
+</li>
+<li>
+<a href='/lab-1-5.php'>Задача №1-1</a>
+</li>
+<li>
+<a href='/lab-2-1.php'>Упражнение 1-6</a>
+</li>
+<li>
+<a href='/lab-2-2.php'>Упражнение 1-7</a>
+</li>
+<li>
+<a href='/lab-2-3.php'>Упражнение 1-8</a>
+</li>
+<li>
+<a href='/lab-2-4.php'>Упражнение 1-9</a>
+</li>
+<li>
+<a href='/lab-2-5.php'>Упражнение 1-10</a>
+</li>
+<li>
+<a href='/lab-2-6.php'>Упражнение 1-11</a>
+</li>
+<li>
+<a href='/lab-2-7.php'>Упражнение 1-12</a>
+</li>
+<li>
+<a href='/lab-2-8.php'>Упражнение 1-13</a>
+</li>
+<li>
+<a href='/lab-2-9.php'>Упражнение 1-14</a>
+</li> 
+<li>
+<a href='/lab-2-10.php'>Задача № 1-2</a>
+</li>
+<li>
+<a href='/lab-2-11.php'>Задача № 1-3</a>
+</li>
+</font>
+</CENTER>
