06ef6fde58033c6619db26d6f80aceb3435d9f3f
Kolesova222 <80149209+Kolesova222@users.noreply.github.com>
2021-12-14 16:07:16 +0500

    ЛР2
    Лр2

--- index.php
+++ index.php
@@ -55,5 +55,24 @@
 <li>
 <a href='/lab-2-11.php'>Задача № 1-3</a>
 </li>
+<CENTER><font color="#FFF0F5"><FONT SIZE="+2"><I><p>♥ Лабораторная работа 2 ♥</p></I></FONT></font></CENTER>
+<li>
+<a href='/lab-2-2-1.php'>Задание № 2-1</a>
+</li>
+<li>
+<a href='/lab-2-2-2.php'>Задание № 2-2</a>
+</li>
+<li>
+<a href='/lab-2-2-3.php'>Задание № 2-3</a>
+</li>
+<li>
+<a href='/lab-2-2-4.php'>Задача № 2.4</a>
+</li>
+<li>
+<a href='/lab-2-2-5.php'>Задача № 2.5</a>
+</li>
+<li>
+<a href='/lab-2-2-6.php'>Задача № 2.6</a>
+</li>
 </font>
 </CENTER>
---
+++ lab-2-2-6.php
@@ -0,0 +1,8 @@
+<p> Колесова Валерия ПИ-322 <p>
+<?php
+include ('2-Library.php');
+Zad ();
+Mas();
+Viv();
+Obr();
+?>
